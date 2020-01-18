c539f015a4cc329709f7d1066be83db9886e636a
Loucas Cubeddu <lcubeddu@outlook.fr>
2020-01-18 16:19:20 +0100

    Commit 2

--- openGL/src/Application.cpp
+++ openGL/src/Application.cpp
@@ -188,27 +188,16 @@ int main(void)
             if (show_demo_window)
                 ImGui::ShowDemoWindow(&show_demo_window);
 
-            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
             {
-                /*static float f = 0.0f;
-                static int counter = 0;*/
+                ImGui::Begin("Framerate");
 
-                ImGui::Begin("Framerate");                          // Create a window called "Hello, world!" and append into it.
+                ImGui::Checkbox("Demo Window", &show_demo_window); 
 
-                //ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-
-                //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
-                //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-                //if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                //    counter++;
-                //ImGui::SameLine();
-                //ImGui::Text("counter = %d", counter);
+                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f-100.0f);
+                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f-100.0f);
 
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
                 ImGui::End();
             }
 
